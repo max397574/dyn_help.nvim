60eceb7f954b1574d2eaced7e910fac7b3510110
max397574 <max.heinzer397@gmx.ch>
2022-03-20 17:37:47 +0100

    tests(utils): added tests for `get_help_tag_lines`

--- tests/utils/util_spec.lua
+++ tests/utils/util_spec.lua
@@ -1,5 +1,5 @@
 ---@diagnostic disable: undefined-global
-describe("Util tests:", function()
+describe("Util tests (option values):", function()
     it("Get an option value", function()
         vim.cmd([[set relativenumber]])
         if vim.fn.has("nvim-0.7.0") == 1 then
@@ -39,6 +39,34 @@ describe("Util tests:", function()
                 "a_random_invalid_option"
             ),
             ""
+        )
+    end)
+end)
+
+describe("Util tests (help tags):", function()
+    it("Get help tag lines", function()
+        local help_tag_lines = require("dynamic_help.utils").get_help_tag_lines(
+            "help"
+        )
+        assert.are.same(
+            help_tag_lines,
+            {
+                "help",
+                "\t\t\t*help* *<Help>* *:h* *:help* *<F1>* *i_<F1>* *i_<Help>*",
+                "<Help>\t\tor",
+                ":h[elp]\t\t\tOpen a window and display the help file in read-only",
+                "\t\t\tmode.  If there is a help window open already, use",
+                "\t\t\tthat one.  Otherwise, if the current window uses the",
+                "\t\t\tfull width of the screen or is at least 80 characters",
+                "\t\t\twide, the help window will appear just above the",
+                "\t\t\tcurrent window.  Otherwise the new window is put at",
+                "\t\t\tthe very top.",
+                "\t\t\tThe 'helplang' option is used to select a language, if",
+                "\t\t\tthe main help file is available in several languages.",
+                "",
+                "\t\t\tType |gO| to see the table of contents.",
+                "",
+            }
         )
     end)
 end)
