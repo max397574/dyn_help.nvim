3ef9a57b96bdf0e6bfb9fb1d4dad836b8cfd1623
max397574 <max.heinzer397@gmx.ch>
2022-03-20 15:46:28 +0100

    fix(tests): fix for 0.6

--- tests/utils/util_spec.lua
+++ tests/utils/util_spec.lua
@@ -1,29 +1,29 @@
 ---@diagnostic disable: undefined-global
--- describe("Example test", function()
---     it("Check if numbers are equal", function()
---         assert.equal(3, 5 - 2)
---     end)
--- end)
 describe("Util tests:", function()
     it("Get an option value", function()
         vim.cmd([[set relativenumber]])
-        if vim.fn.has("nvim-0.7.0") then
+        if vim.fn.has("nvim-0.7.0") == 1 then
             assert.equals(
                 require("dynamic_help.utils").get_option_value("relativenumber"),
                 "true"
             )
         end
     end)
+    it("Get an 0.6 compatible option value", function()
+        vim.cmd([[set shiftwidth=4]])
+        assert.equals(
+            require("dynamic_help.utils").get_option_value("shiftwidth"),
+            "4"
+        )
+    end)
     it("Get an option default value", function()
         vim.cmd([[set relativenumber]])
-        if vim.fn.has("nvim-0.7.0") then
-            assert.equals(
-                require("dynamic_help.utils").get_default_option_value(
-                    "relativenumber"
-                ),
-                "false"
-            )
-        end
+        assert.equals(
+            require("dynamic_help.utils").get_default_option_value(
+                "relativenumber"
+            ),
+            "false"
+        )
     end)
     it("Get an invalid option default value", function()
         assert.equals(
