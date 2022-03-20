e05028af7ada66cf47fe1d769943d12c67868cce
max397574 <max.heinzer397@gmx.ch>
2022-03-20 15:45:40 +0100

    feat(utils): make 0.6 compatible

--- lua/dynamic_help/utils.lua
+++ lua/dynamic_help/utils.lua
@@ -6,6 +6,9 @@ local utils = {}
 ---@return string value The value of the option as string or empty string
 function utils.get_option_value(option)
     local ok, res = pcall(vim.api.nvim_get_option_value, option, {})
+    if not vim.fn.has("nvim-0.7.0") == 1 then
+        ok, res = pcall(vim.api.nvim_get_option, option)
+    end
     if ok then
         return tostring(res)
     else
