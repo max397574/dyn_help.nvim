538f5d5cf316c27724e57f09a2fd77ee3c6fe0ad
max397574 <max.heinzer397@gmx.ch>
2022-03-20 15:00:12 +0100

    feat(utils): added function to get default value

--- lua/dynamic_help/utils.lua
+++ lua/dynamic_help/utils.lua
@@ -13,4 +13,17 @@ function utils.get_option_value(option)
     end
 end
 
+--- Gets the default value of an option
+--- It's safe to use this with an invalid name
+---@param option string Name of option
+---@return string value The default value of the option or empty string
+function utils.get_default_option_value(option)
+    local ok, res = pcall(vim.api.nvim_get_option_info, option)
+    if ok then
+        return tostring(res.default)
+    else
+        return ""
+    end
+end
+
 return utils
--- tests/utils/util_spec.lua
+++ tests/utils/util_spec.lua
@@ -14,6 +14,25 @@ describe("Util tests:", function()
             )
         end
     end)
+    it("Get an option default value", function()
+        vim.cmd([[set relativenumber]])
+        if vim.fn.has("0.7") then
+            assert.equals(
+                require("dynamic_help.utils").get_default_option_value(
+                    "relativenumber"
+                ),
+                "false"
+            )
+        end
+    end)
+    it("Get an invalid option default value", function()
+        assert.equals(
+            require("dynamic_help.utils").get_default_option_value(
+                "a_random_invalid_option"
+            ),
+            ""
+        )
+    end)
     it("Get an invalid option value", function()
         assert.equals(
             require("dynamic_help.utils").get_option_value(
